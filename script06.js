7e796fbc702d3d61df1f83904f8011c2f6f058c9
MihPar <9496260@gmail.com>
2023-10-28 16:41:40 +0300

    2810231641

--- script06.js
+++ script06.js
@@ -1,14 +1,18 @@
 function solve(arr) {
-	let unique = Array.from(new Set(arr));
-  let result = [];
+// 	let unique = Array.from(new Set(arr));
+//   let result = [];
   
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (unique.includes(arr[i])) {
-      result.unshift(arr[i]);
-      unique = unique.filter(num => num !== arr[i]);
-    }
-  }
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     if (unique.includes(arr[i])) {
+//       result.unshift(arr[i]);
+//       unique = unique.filter(num => num !== arr[i]);
+//     }
+//   }
   
-  return result;
+//   return result;
+
+	let newArr = [...new Set(arr.reverse())].reverse()
+	return newArr
+	
 }
 console.log(solve([3,4,4,3,6,3]))
